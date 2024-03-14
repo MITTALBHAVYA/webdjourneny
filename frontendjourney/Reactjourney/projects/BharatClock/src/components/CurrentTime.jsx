f364cf11212bb3d79d1c34f85209f6a9d43b93ef
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-03-14 09:46:54 +0530

    newcommit

--- frontendjourney/Reactjourney/projects/BharatClock/src/components/CurrentTime.jsx
+++ frontendjourney/Reactjourney/projects/BharatClock/src/components/CurrentTime.jsx
@@ -1,8 +1,18 @@
-let CurrentTime = () =>{
+import { useEffect, useState } from "react";
 
-    let time = new Date();
+let CurrentTime = () => {
+    const [time,setTime]=useState(new Date);
+    useEffect(()=>{
+        console.log("Interval has been setup");
+        const intervalId = setInterval(()=>{
+            setTime(new Date());
+        },1000);
 
-
-    return <p>This is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()} </p>
+        return ()=>{
+            clearInterval(intervalId);
+            console.log("Cancelled the interval");
+        }
+    },[]);
+    return (<p>This is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()} </p>)
 };
 export default CurrentTime;
