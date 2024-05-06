f950202af269aca58821f8ea3405112b83dd4c56
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-05-06 14:55:58 +0530

    JWT learning

--- backendjourney/peepcodingBackend/foodApp/Routers/authHelpers.js
+++ backendjourney/peepcodingBackend/foodApp/Routers/authHelpers.js
@@ -1,6 +1,17 @@
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../../secreats');
+
 function ProtectRoute(req,res,next){
-    if(req.cookies.IsLoggedIn){
-        next();
+    if(req.cookies.login){
+        let isVerified = jwt.verify(req.cookies.login,JWT_KEY);
+        if(isVerified){
+            next();
+        }
+        else{
+            return res.json({
+                message:"User is not valid"
+            })
+        }
     }
     else{
         return res.json({
